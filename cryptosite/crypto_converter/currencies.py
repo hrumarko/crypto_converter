5fd5a69957f7c5043737a9022c0dacae6f50e452
Vadym <ssadypic@gmail.com>
2022-12-04 17:48:37 +0200

    Calculation of tokens available in api and UAH

--- cryptosite/crypto_converter/currencies.py
+++ cryptosite/crypto_converter/currencies.py
@@ -1,14 +1,46 @@
+from pycoingecko import CoinGeckoAPI
+
+
 def get_currencies():
     currencies = [
         ('BTC', 'BTC'),
         ('UAH', 'UAH'),
         ('ETH', 'ETH'),
-        ('SOL', 'SOL'),
-        ('USDT', 'USDT'),
-        ('DOGE', 'DOGE'),
+        ('USD', 'USD'),
         ('BNB', 'BNB'),
+        ('LTC', 'LTC'),
         ('XRP', 'XRP'),
-        ('ADA', 'ADA'),
-        ('TRX', 'TRX'),
+        ('BCH', 'BCH'),
+        ('EOS', 'EOS'),
     ]
     return currencies
+
+
+def translate_crypto(crypto):
+    cur = {
+        'BTC': 'bitcoin',
+        'UAH': 'uah',
+        'ETH': 'ethereum',
+        'USD': 'tether',
+        'BNB': 'binancecoin',
+        'LTC': 'binance-peg-litecoin',
+        'XRP': 'binance-peg-xrp',
+        'BCH': 'binance-peg-bitcoin-cash',
+        'EOS': 'binance-peg-eos'
+    }
+    return cur[crypto]
+
+
+cg = CoinGeckoAPI()
+
+
+def get_price(amount, give, take):
+    if give == 'uah':
+        take = translate_crypto(take.upper())
+        price = cg.get_price(ids=take, vs_currencies=give)[take][give]
+        price = float(amount) * (1/price)
+        return price
+    price = cg.get_price(ids=give, vs_currencies=take)[give][take]
+    price = price * float(amount)
+    print(price)
+    return price
--- cryptosite/crypto_converter/views.py
+++ cryptosite/crypto_converter/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import ConvertForm
-
+from . import currencies
 
 
 def index(request):
-    form = ConvertForm()
+    if request.method == 'POST':
+        form = ConvertForm(request.POST)
+        amount = request.POST.get('amount')
+        give = request.POST.get('give')
+        take = request.POST.get('take').lower()
+        give = currencies.translate_crypto(give)
+        print(f'give = {give}, take = {take}')
+        price = currencies.get_price(amount, give, take)
+        
+    else:
+        amount = 0
+        form = ConvertForm()
+
     ctx = {
         'form': form,
+        'price': price,
     }
 
     return render(request, 'crypto_converter/index.html', ctx)

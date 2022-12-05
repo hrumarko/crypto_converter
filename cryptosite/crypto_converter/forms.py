6e02154fe6b39ab1752adb605e2420cee801738e
Vadym <ssadypic@gmail.com>
2022-12-05 10:55:10 +0200

    CSS style, swap tokens

--- cryptosite/crypto_converter/forms.py
+++ cryptosite/crypto_converter/forms.py
@@ -3,6 +3,23 @@ from . import currencies
 
 
 class ConvertForm(forms.Form):
-    amount = forms.DecimalField(min_value=0)
-    give = forms.ChoiceField(choices=currencies.get_currencies())
-    take = forms.ChoiceField(choices=currencies.get_currencies())
+    amount = forms.DecimalField(
+        label='',
+        min_value=0,
+        widget=forms.NumberInput(attrs={
+            'class': 'amount',
+            'placeholder': 'Input your amount',
+        })
+    )
+    give = forms.ChoiceField(
+        choices=currencies.get_currencies(),
+        widget=forms.Select(attrs={
+            'class': 'give-ch'
+        })
+    )
+    take = forms.ChoiceField(
+        choices=currencies.get_currencies(),
+        widget=forms.Select(attrs={
+            'class': 'take-ch'
+        })
+    )
--- cryptosite/crypto_converter/views.py
+++ cryptosite/crypto_converter/views.py
@@ -6,14 +6,23 @@ from . import currencies
 
 def index(request):
     if request.method == 'POST':
-        form = ConvertForm(request.POST)
-        amount = request.POST.get('amount')
-        give = request.POST.get('give')
-        take = request.POST.get('take').lower()
-        give = currencies.translate_crypto(give)
-        print(f'give = {give}, take = {take}')
-        price = currencies.get_price(amount, give, take)
-
+        if 'swap_cur' in request.POST:
+            amount = request.POST.get('amount')
+            give = request.POST.get('give')
+            take = request.POST.get('take')
+            form = ConvertForm(initial={
+                'amount': request.POST.get('amount'),
+                'give': take,
+                'take': give
+            })
+            price = request.POST.get('price')
+        if 'get_price' in request.POST:
+            form = ConvertForm(request.POST)
+            amount = request.POST.get('amount')
+            give = request.POST.get('give')
+            take = request.POST.get('take').lower()
+            give = currencies.translate_crypto(give)
+            price = currencies.get_price(amount, give, take)
     else:
         price = 0
         form = ConvertForm()

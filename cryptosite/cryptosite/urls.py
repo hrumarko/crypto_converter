44f358dd95274d672959d58c27a0ee588a17056b
Vadym <ssadypic@gmail.com>
2022-12-05 15:13:27 +0200

    added gebug toolbar

--- cryptosite/cryptosite/urls.py
+++ cryptosite/cryptosite/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from cryptosite import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('crypto_converter.urls'))
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [
+        path('__debug__/', include('debug_toolbar.urls')),
+    ] + urlpatterns
